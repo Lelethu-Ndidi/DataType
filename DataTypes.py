bedcbcac8fa703e90fb4971cfa10dd94df575420
Wade <lelethundidi@gmail.com>
2020-03-10 11:15:07 +0200

    Addition DataTypes

--- DataTypes.py
+++ DataTypes.py
@@ -11,14 +11,20 @@ Number2 = 5.8         # float data type
 Number3 = 3e10j       # complex data type
 # checking whether the data type is what I said it is .
 print(type(Number1), type(Number2), type(Number3))
-#using list
-mylist = [23, 82 , 67, 12, 56]     # Numbers
+# Using list
+mylist = [23, 82 , 67, 12, 56]     # Data Type list
 mylist.insert(4,21)                # Insert in position 4, number 21
 print(mylist)                      # Printing the new list
 mylist.reverse()                   # Reverses my list from the last to the first
 print(mylist)
 # Testing the Different data types: The difference is (Curly Bracket and normal bracket)
 cars = {"Aston Martin", "VW", "Ford", "BMW", "Nissan"}
-print(type(cars))                                           #Data Type = Set
+print(type(cars))                                           # Data Type = Set
 cars = ("Aston Martin", "VW", "Ford", "BMW", "Nissan")
 print(type(cars))                                           # Data Type = Tuple
+# Testing Boolean
+late = True
+print(type(late))     # Displaying Data Type : Bool
+
+myDict = {"1:" "Oslin", "2:" "Kayleen", "3:" "Nazneen","4:" "AK"}
+print(type(myDict))                                                      #Data Type : Set
